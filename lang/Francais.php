75df566cf6f5a9193a54984de1bd5cec8ac3a51e
David Keller <david.keller@litchis.fr>
2016-05-08 10:53:56 +0200

    Initial commit.

---
+++ lang/Francais.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_help_did_nsfw = array(
+'List info'	=>	'BBCode utilisable pour masquer ou voir des 
+messages.',
+'Spoiler'	=>	'Ce texte est masquÃ© jusqu\'Ã être cliquÃ©.',
+'str_NSFW'	=>	'NSFW',
+'str_Hide'	=>	'Masquer',
+'str_Show'	=>	'Voir',
+);
+
+?>
